ac81977db90d127d767564286068fcc8456cb581
Melville Ezeogu <Melville.Ezeogu@cfpb.gov>
2016-02-10 13:23:09 -0500

    updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env bash
 
-sudo yum -y groupinstall 'Development Tools'
-sudo yum -y install yum localinstall http://yum.postgresql.org/9.4/redhat/rhel-6-x86_64/pgdg-centos94-9.4-1.noarch.rpm
-sudo yum -y install postgresql9*
-sudo yum -y install tree
-
-
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-if [ "$SCRIPTPATH" = "/tmp" ] ; then
+if [ "$SCRIPTPATH" = "/home" ] ; then
        SCRIPTPATH=/vagrant
    fi
   
   mkdir -p $HOME/rpmbuild/{BUILD,RPMS,SOURCES,SRPMS}
+  ln -sf $SCRIPTPATH/SPECS $HOME/rpmbuild/SPECS
 echo '%_topdir '$HOME'/rpmbuild' > $HOME/.rpmmacros
 cd $HOME/rpmbuild/SOURCES
 wget https://github.com/keithf4/pg_partman/archive/master.tar.gz
