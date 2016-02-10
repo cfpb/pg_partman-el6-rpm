b6d2459f17bffb33fc75a0483bf3368007f32dc6
Melville Ezeogu <Melville.Ezeogu@cfpb.gov>
2016-02-10 09:09:38 -0500

    initial commits

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,12 +12,11 @@ sudo yum -y install tree
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-if [ "$SCRIPTPATH" = "/Users/ezeogum/Projects/rpm-git-clones/pg_partman-rpm" ] ; then
+if [ "$SCRIPTPATH" = "/tmp" ] ; then
        SCRIPTPATH=/vagrant
    fi
   
   mkdir -p $HOME/rpmbuild/{BUILD,RPMS,SOURCES,SRPMS}
- ln -sf /Users/ezeogum/Projects/pg_mon.task/SPECS $HOME/rpmbuild/SPECS
 echo '%_topdir '$HOME'/rpmbuild' > $HOME/.rpmmacros
 cd $HOME/rpmbuild/SOURCES
 wget https://github.com/keithf4/pg_partman/archive/master.tar.gz
